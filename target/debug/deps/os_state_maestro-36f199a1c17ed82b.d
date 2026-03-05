74566930e4b50d5e7a64d4222d581325040dd48a
alisio85 <alessio.zanoni85@gmail.com>
2026-03-05 09:40:50 +0100

    update

--- target/debug/deps/os_state_maestro-36f199a1c17ed82b.d
+++
@@ -1,5 +0,0 @@
-F:\os_state_maestro\target\debug\deps\os_state_maestro-36f199a1c17ed82b.d: src\lib.rs
-
-F:\os_state_maestro\target\debug\deps\os_state_maestro-36f199a1c17ed82b.exe: src\lib.rs
-
-src\lib.rs:
--- target/debug/deps/os_state_maestro-988a44d635f6a8e9.d
+++
@@ -1,9 +0,0 @@
-F:\os_state_maestro\target\debug\deps\os_state_maestro-988a44d635f6a8e9.d: src\lib.rs Cargo.toml
-
-F:\os_state_maestro\target\debug\deps\libos_state_maestro-988a44d635f6a8e9.rmeta: src\lib.rs Cargo.toml
-
-src\lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
